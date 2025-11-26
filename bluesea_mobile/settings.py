7d8f6bbb0ba6340c528f558639d56aeb5497bb29
Right Fix <righteousnessude@gmail.com>
2025-11-26 04:26:41 +0100

    Update settings.py

--- bluesea_mobile/settings.py
+++ bluesea_mobile/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DEBUG')
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
