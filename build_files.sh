5547d5c9ed8735e0e25bb3833c7f32f2cdf49eaf
Right Fix <righteousnessude@gmail.com>
2025-11-26 03:31:55 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Install dependencies first (Vercel already does this, but safe to repeat)
 pip install -r requirements.txt
+
+# Now run Django commands
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
